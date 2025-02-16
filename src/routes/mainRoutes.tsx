1e6decb25cc8470e0696031d611c57d25c3780ce
Narek Aleksanyan <narek.alexanyan.96@gmail.com>
2025-02-16 18:27:47 +0400

    Created main section

--- src/routes/mainRoutes.tsx
+++ src/routes/mainRoutes.tsx
@@ -1,8 +1,9 @@
 import { Route } from "react-router";
 import MainLayout from "../layouts/MainLayout";
+import { GameSelection } from "../pages/GameSelection";
 
 export const mainRoutes = (
   <Route element={<MainLayout />}>
-    <Route index element={<div>Dashboard</div>} />
+    <Route index element={<GameSelection />} />
   </Route>
 );
